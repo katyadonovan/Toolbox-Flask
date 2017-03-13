4631b3a17f56e267b689ab6a753448a2ad3f812a
Katya <katya@computer.com>
2017-03-13 15:33:20 -0400

    Turning in Toolbox #3!

--- catseverywhere.py
+++ catseverywhere.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
-"""
+
+
 @app.route('/')
 def hello_world():
-    author ="Me"
-    name = "You"
-    return 'Hello World!"""
+    return render_template('index.html')
 
-@app.rout('/signup', methods = ['POST'])
+@app.route('/signup', methods = ['POST', 'GET'])
 def signup():
-    name=request.form['name']
-    age = request.form['age']
-    print("Hello'"+ name +"'")
-    return redirect('/')
+    error = None
+    if request.method == 'POST':
+        if(request.form['name'] != '' and request.form['age'] != '' and request.form['fav ninja'] != ''):
+            name = request.form['name']
+            age = request.form['age']
+        else:
+            error = 'Please fill out everything!'
+    return render_template("result.html",name = name, age = age,error=error)
 
 if __name__ == '__main__':
     app.run()
